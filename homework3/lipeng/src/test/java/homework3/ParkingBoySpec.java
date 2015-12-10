5a7a02a7695b900b603b7cbbf878cbef041c5dd2
Peng.Li <peng.li@rea-group.com>
2015-12-10 12:56:39 +0800

    group

--- homework3/lipeng/src/test/java/homework3/ParkingBoySpec.java
+++ homework3/lipeng/src/test/java/homework3/ParkingBoySpec.java
@@ -26,19 +26,23 @@ public class ParkingBoySpec {
             it("可以管理多个停车场", () -> {
                 assertThat(boy.getParkingLots()).containsExactly(parkingLot1, parkingLot2);
             });
-            it("可以帮忙停车", () -> {
-                String ticket = boy.park(car);
-                assertThat(ticket).isNotNull();
+            describe("停车", () -> {
+                it("可以帮忙停车", () -> {
+                    String ticket = boy.park(car);
+                    assertThat(ticket).isNotNull();
+                });
+                it("如果没有空位,则停不了", () -> {
+                    boy = new ParkingBoy(new ParkingLot(0));
+                    String ticket = boy.park(car);
+                    assertThat(ticket).isNull();
+                });
             });
-            it("如果没有空位,则停不了", () -> {
-                boy = new ParkingBoy(new ParkingLot(0));
-                String ticket = boy.park(car);
-                assertThat(ticket).isNull();
-            });
-            it("可以帮忙取车", () -> {
-                String ticket = boy.park(car);
-                Car car = boy.unpark(ticket);
-                assertThat(car).isNotNull();
+            describe("取车", () -> {
+                it("可以帮忙取车", () -> {
+                    String ticket = boy.park(car);
+                    Car car = boy.unpark(ticket);
+                    assertThat(car).isNotNull();
+                });
             });
         });
 
