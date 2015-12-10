443cda1ea3e8055de557ac3178f451d6632fffde
Peng.Li <peng.li@rea-group.com>
2015-12-10 12:42:39 +0800

    improve tests

--- homework3/lipeng/src/test/java/homework3/ParkingLotSpec.java
+++ homework3/lipeng/src/test/java/homework3/ParkingLotSpec.java
@@ -9,18 +9,26 @@ import static org.assertj.core.api.Assertions.assertThat;
 @RunWith(Spectrum.class)
 public class ParkingLotSpec {
 
+    private ParkingLot parkingLot;
+
+    private Car car;
+
     {
+
+        beforeEach(() -> {
+            parkingLot = new ParkingLot(2);
+            car = new Car();
+        });
 
         describe("停车场", () -> {
             describe("停车", () -> {
                 it("只要没满可以停车", () -> {
-                    ParkingLot parkingLot = new ParkingLot(2);
-                    String ticket = parkingLot.park(new Car());
+                    String ticket = parkingLot.park(car);
                     assertThat(ticket).isNotNull();
                     assertThat(parkingLot.getAvailableSpaces()).isEqualTo(1);
                 });
                 it("满了就不能停了", () -> {
-                    ParkingLot parkingLot = new ParkingLot(1);
+                    parkingLot.park(new Car());
                     parkingLot.park(new Car());
                     String ticket = parkingLot.park(new Car());
                     assertThat(ticket).isNull();
@@ -29,19 +37,15 @@ public class ParkingLotSpec {
             });
             describe("取车", () -> {
                 it("可以取回自己的车", () -> {
-                    ParkingLot parkingLot = new ParkingLot(2);
-                    Car car = new Car();
                     String ticket = parkingLot.park(car);
                     Car returnedCar = parkingLot.unpark(ticket);
                     assertThat(returnedCar).isEqualTo(car);
                 });
                 it("无票不能取车", () -> {
-                    ParkingLot parkingLot = new ParkingLot(2);
                     Car returnedCar = parkingLot.unpark(null);
                     assertThat(returnedCar).isNull();
                 });
                 it("无效票不能取车", () -> {
-                    ParkingLot parkingLot = new ParkingLot(2);
                     Car returnedCar = parkingLot.unpark("invalid-ticket");
                     assertThat(returnedCar).isNull();
                 });
@@ -49,4 +53,5 @@ public class ParkingLotSpec {
         });
 
     }
+
 }
